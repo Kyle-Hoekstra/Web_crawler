63b95e9e68b4eeb157c765297d7b5d062d73442b
Kyle-Hoekstra <kyle.m.s.hoekstra@gmail.com>
2024-01-25 21:05:53 +0000

    Basic web crawler built following tutorial

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
+import csv
 
+# initialize the list of discovered urls
+# with the first page to visit
+urls = ["https://scrapeme.live/shop/"]
+#Array to store dict entries of products and their data
+products = []
 
-print("Importing modules...")
+# until all pages have been visited
+while len(urls) != 0:
+    # get the page to visit from the list
+    current_url = urls.pop()
+    
+    # crawling logic
+    response = requests.get(current_url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-modules = ['requests', 'bs4']
-for name in modules:
-    if name not in sys.modules:
-        print('You have not imported the {} module'.format(name))
+    link_elements = soup.select("a[href]")
+    for link_element in link_elements:
+        url = link_element['href']
+        if "https://scrapeme.live/shop" in url:
+            urls.append(url)
 
-print("Docker is magic")
+    print("Number of Urls remaining: " + str(len(urls)))
+
+    #Extract specific data from products and store it
+    product = {}
+    product["url"] = current_url 
+    product["image"] = soup.select_one(".wp-post-image")["src"]
+    #product["title"] = soup.select_one(".product_title").text() 
+    product["price"] = soup.select_one(".price")
+
+    #place product in products array for CSV to read through
+    products.append(product)
+
+#CSV output
+with open('products.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+
+    #Fill CSV file
+    for product in products:
+        writer.writerow(product.values())
+
